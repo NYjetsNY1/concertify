19ffe5811d1891ad30579737c38bf9e533bffac9
Brendan Broderick <brendanbroderick95@gmail.com>
2017-12-10 17:50:06 -0600

    Remove title requirement

--- src/routes/login/Login.test.js
+++ src/routes/login/Login.test.js
@@ -6,23 +6,23 @@ import Login from './Login';
 describe('Home', () => {
   test('renders correct content', () => {
     const wrapper = renderer
-    .create(
-      <App context={{ insertCss: () => {}, fetch: () => {} }}>
-        <Login></Login>
-      </App>,
-    )
+      .create(
+        <App context={{ insertCss: () => {}, fetch: () => {} }}>
+          <Login />
+        </App>,
+      )
       .toJSON();
 
-      expect(wrapper.type).toBe('div');
-      expect(wrapper.props.className).toContain('root');
-      expect(wrapper.children.length).toBeGreaterThanOrEqual(2);
+    expect(wrapper.type).toBe('div');
+    expect(wrapper.props.className).toContain('root');
+    expect(wrapper.children.length).toBeGreaterThanOrEqual(2);
 
-      let aboutContainter = wrapper.children[0];
-      expect(aboutContainter.type).toBe('a');
-      expect(aboutContainter.props.href).toBe('/about');
+    const aboutContainter = wrapper.children[0];
+    expect(aboutContainter.type).toBe('a');
+    expect(aboutContainter.props.href).toBe('/about');
 
-      let playlistContainter = wrapper.children[1];
-      expect(playlistContainter.type).toBe('a');
-      expect(playlistContainter.props.href).toBe('/auth');
+    const playlistContainter = wrapper.children[1];
+    expect(playlistContainter.type).toBe('a');
+    expect(playlistContainter.props.href).toBe('/auth');
   });
 });
